8dfc2a329e786b403b3bab1b0d81eab464b6ef87
Tom Carter <cartertj1@cardiff.ac.uk>
2021-04-14 16:12:41 +0100

    Added clickable listView for daily weather overview

--- app/src/main/java/com/example/app_coursework/MainFragment.java
+++ app/src/main/java/com/example/app_coursework/MainFragment.java
@@ -4,17 +4,55 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class MainFragment extends Fragment {
+import java.lang.reflect.Array;
+import java.text.DateFormat;
+import java.text.DateFormatSymbols;
+import java.util.ArrayList;
+
+public class MainFragment extends Fragment implements AdapterView.OnItemClickListener {
+
+    ListView listView;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_main, container, false);
 
+        listView = (ListView) v.findViewById(R.id.main_weather_list);
+
+        ArrayList<String> arrayList = new ArrayList<>();
+
+        arrayList.add("Monday");
+        arrayList.add("Tuesday");
+        arrayList.add("Wednesday");
+        arrayList.add("Thursday");
+        arrayList.add("Friday");
+        arrayList.add("Saturday");
+        arrayList.add("Sunday");
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getActivity(),
+                android.R.layout.simple_list_item_1,
+                arrayList);
+
+        listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemClickListener(this);
+
         return v;
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        String day = parent.getItemAtPosition(position).toString();
+        Toast.makeText(getActivity(), day, Toast.LENGTH_SHORT).show();
     }
 }
