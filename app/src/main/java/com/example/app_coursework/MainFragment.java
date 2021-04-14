9aca9fd76484ea6411b86b6410c3186c26da37ae
Tom Carter <cartertj1@cardiff.ac.uk>
2021-04-14 17:41:04 +0100

    Gets weather data as JSON from tomorrow.io

--- app/src/main/java/com/example/app_coursework/MainFragment.java
+++ app/src/main/java/com/example/app_coursework/MainFragment.java
@@ -1,6 +1,7 @@
 package com.example.app_coursework;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,13 +14,25 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import java.lang.reflect.Array;
-import java.text.DateFormat;
-import java.text.DateFormatSymbols;
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainFragment extends Fragment implements AdapterView.OnItemClickListener {
 
+    private RequestQueue requestQueue;
     ListView listView;
 
     @Nullable
@@ -27,10 +40,50 @@ public class MainFragment extends Fragment implements AdapterView.OnItemClickLis
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_main, container, false);
 
+//        API
+        requestQueue = Volley.newRequestQueue(getActivity());
+
+        String apikey = "z6N3a8QW0Cwy80k9sTxvPNHCGqvRFq5f";
+        double[] location = {40.758, -73.9855};
+        String[] fields = {"temperature"};
+        String units = "metric";
+        String[] timestamps = {"current"};
+        String timezone = "America/New_York";
+
+        String url = "https://api.tomorrow.io/v4/timelines" + "?apikey=" + apikey + "&location=" + location[0] + "," + location[1] + "&fields=" + fields[0] + "&units=" + units;
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                Request.Method.GET, //The type of request, e.g., GET, POST, DELETE, PUT, etc.
+                url, //as defined above
+                null, //data to send with the request, none in this case
+                new Response.Listener<JSONObject>() { //onsuccess
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        //access to methods on a JSONObject requires JSONException errors to be
+                        //handled in some way - such as surrounding the code in a try-catch block
+                        try {
+                            Log.d("RESPONSE", response.toString(2)); //prints the response to LogCat
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() { //onerror
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        if (error.getMessage() != null)
+                            Log.d("ERROR", error.getMessage()); //prints the error message to LogCat
+                    }
+                }
+        );
+
+        requestQueue.add(jsonObjectRequest);
+
+
+//       List
         listView = (ListView) v.findViewById(R.id.main_weather_list);
 
         ArrayList<String> arrayList = new ArrayList<>();
-
         arrayList.add("Monday");
         arrayList.add("Tuesday");
         arrayList.add("Wednesday");
