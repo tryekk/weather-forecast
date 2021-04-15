a81958e0afe6f3101d1d173858ae64e20ddd3331
Tom Carter <cartertj1@cardiff.ac.uk>
2021-04-15 18:46:29 +0100

    Added toolbar to addLocation

--- app/src/main/java/com/example/app_coursework/AddLocation.java
+++ app/src/main/java/com/example/app_coursework/AddLocation.java
@@ -1,6 +1,7 @@
 package com.example.app_coursework;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
@@ -11,6 +12,10 @@ public class AddLocation extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_location);
+
+        // Add toolbar to the appbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         FragmentManager fragmentManager = getSupportFragmentManager();
         fragmentManager.beginTransaction()
