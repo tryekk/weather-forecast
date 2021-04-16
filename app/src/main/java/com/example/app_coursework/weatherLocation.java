129fbaf45dd335f636ddc06d4511dd73fd360a93
Tom Carter <cartertj1@cardiff.ac.uk>
2021-04-16 17:47:07 +0100

    Added DAO for weatherLocation

--- app/src/main/java/com/example/app_coursework/weatherLocation.java
+++ app/src/main/java/com/example/app_coursework/weatherLocation.java
@@ -1,12 +1,28 @@
 package com.example.app_coursework;
 
+import androidx.room.ColumnInfo;
+import androidx.room.PrimaryKey;
+
 public class weatherLocation {
+
+    @PrimaryKey
     private String name;
+    @ColumnInfo
+    private double[] coordinates;
+    @ColumnInfo(defaultValue = "false")
     private boolean isChosen;
 
     public weatherLocation(String name, boolean isChosen) {
         this.name = name;
         this.isChosen = isChosen;
+    }
+
+    public double[] getCoordinates() {
+        return coordinates;
+    }
+
+    public void setCoordinates(double[] coordinates) {
+        this.coordinates = coordinates;
     }
 
     public String getName() {
