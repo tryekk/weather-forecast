728de29c83331e6c71b1f686f1e4f750c2078383
Tom Carter <cartertj1@cardiff.ac.uk>
2021-04-22 10:36:29 +0100

    Formatted time and removed unused imports

--- app/src/main/java/com/example/app_coursework/DailyWeatherFragment.java
+++ app/src/main/java/com/example/app_coursework/DailyWeatherFragment.java
@@ -1,40 +1,25 @@
 package com.example.app_coursework;
 
-import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.Volley;
 import com.example.app_coursework.adapter.DailyWeatherAdapter;
-import com.example.app_coursework.adapter.HourlyWeatherAdapter;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
-import java.util.Objects;
-import java.util.TimeZone;
+
 
 public class DailyWeatherFragment extends Fragment implements AdapterView.OnItemClickListener {
 
--- app/src/main/java/com/example/app_coursework/HourlyWeatherFragment.java
+++ app/src/main/java/com/example/app_coursework/HourlyWeatherFragment.java
@@ -1,12 +1,6 @@
 package com.example.app_coursework;
 
-import android.content.SharedPreferences;
-import android.content.pm.PackageManager;
-import android.location.Location;
-import android.os.Build;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,38 +8,15 @@ import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.annotation.RequiresApi;
-import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.recyclerview.widget.RecyclerView;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.Volley;
-import com.example.app_coursework.adapter.DailyWeatherAdapter;
 import com.example.app_coursework.adapter.HourlyWeatherAdapter;
-import com.example.app_coursework.adapter.LocationArrayAdapter;
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.tasks.OnSuccessListener;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.TimeZone;
-
-import static android.Manifest.permission.ACCESS_FINE_LOCATION;
-import static android.Manifest.permission.SYSTEM_ALERT_WINDOW;
 
 public class HourlyWeatherFragment extends Fragment {
 
@@ -73,7 +44,7 @@ public class HourlyWeatherFragment extends Fragment {
         try {
             JSONArray arr = response.getJSONObject("data").getJSONArray("timelines").getJSONObject(0).getJSONArray("intervals");
             for (int i = 0; i < arr.length(); i++) {
-                String[] dateFormatted = arr.getJSONObject(i).getString("startTime").split("T");
+                String[] dateFormatted = arr.getJSONObject(i).getString("startTime").split("T|:0");  // Get time from UTC timestamp string
                 weatherList.add(dateFormatted[1] + "," +
                         String.valueOf(arr.getJSONObject(i).getJSONObject("values").getDouble("temperature")));
             }
