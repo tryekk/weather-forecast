52e1fb73ee498b23be8d49c29dcbc0c50f321c74
Tom Carter <cartertj1@cardiff.ac.uk>
2021-04-23 09:45:22 +0100

    Updated time regex

--- app/src/main/java/com/example/app_coursework/HourlyWeatherFragment.java
+++ app/src/main/java/com/example/app_coursework/HourlyWeatherFragment.java
@@ -45,7 +45,7 @@ public class HourlyWeatherFragment extends Fragment {
         try {
             JSONArray arr = response.getJSONObject("data").getJSONArray("timelines").getJSONObject(0).getJSONArray("intervals");
             for (int i = 0; i < arr.length(); i++) {
-                String[] dateFormatted = arr.getJSONObject(i).getString("startTime").split("T|:0");  // Get time from UTC timestamp string
+                String[] dateFormatted = arr.getJSONObject(i).getString("startTime").split("T|:00");  // Get time from UTC timestamp string
                 weatherList.add(dateFormatted[1] + "," +
                         String.valueOf(arr.getJSONObject(i).getJSONObject("values").getDouble("temperature")) + "    " +
                         String.valueOf(arr.getJSONObject(i).getJSONObject("values").getDouble("weatherCode")));  // TODO requires translation
