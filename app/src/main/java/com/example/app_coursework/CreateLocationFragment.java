5cdfe74cee1e79e9bf7440f06000b8df8f0375a9
Tom Carter <cartertj1@cardiff.ac.uk>
2021-04-26 15:34:59 +0100

    Data entered on create location screen is added to room database

--- app/src/main/java/com/example/app_coursework/CreateLocationFragment.java
+++ app/src/main/java/com/example/app_coursework/CreateLocationFragment.java
@@ -5,19 +5,57 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.app_coursework.adapter.LocationArrayAdapter;
+import com.example.app_coursework.database.WeatherLocation;
+import com.example.app_coursework.database.WeatherLocationDatabase;
+
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 
 public class CreateLocationFragment extends Fragment {
 
+    private WeatherLocationDatabase weatherLocationDatabase = WeatherLocationDatabase.getInstance(getContext());;
+    private ExecutorService executorService;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_create_location, container, false);
+
+        EditText input = (EditText) v.findViewById(R.id.create_location_text);
+        EditText inputCoordinates = (EditText) v.findViewById(R.id.create_location_text_coordinates);
+
+        Button submitButton = v.findViewById(R.id.button_save_location);
+        submitButton.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                // Create background thread
+                executorService = Executors.newSingleThreadExecutor();
+                executorService.execute(new Runnable() {
+                    @Override
+                    public void run() {
+                        // Add user input to database
+                        weatherLocationDatabase.weatherLocationDAO().addWeatherLocation(
+                                new WeatherLocation(input.getText().toString(), inputCoordinates.getText().toString(), 0));
+                        // Back to add location screen
+                        Intent intent = new Intent();
+                        intent.setClass(getActivity(), AddLocationActivity.class);
+                        startActivity(intent);
+                    }
+                });
+            }
+        });
 
         return v;
     }
