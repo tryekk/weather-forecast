075b8086a5e8f61a2dcc326309f950439704f38f
Tom Carter <thomascsrter01@me.com>
2021-07-08 18:05:55 +0100

    Draw triangle in OpenGL

--- app/src/main/java/com/example/app_coursework/opengl/OpenGLRenderer.java
+++ app/src/main/java/com/example/app_coursework/opengl/OpenGLRenderer.java
@@ -8,9 +8,14 @@ import javax.microedition.khronos.opengles.GL10;
 
 public class OpenGLRenderer implements GLSurfaceView.Renderer {
 
+    private Triangle mTriangle;
+
     @Override
     public void onSurfaceCreated(GL10 gl, EGLConfig config) {
         GLES20.glClearColor(1f, 0, 0, 1);
+
+        // initialize a triangle
+        mTriangle = new Triangle();
     }
 
     @Override
@@ -21,5 +26,21 @@ public class OpenGLRenderer implements GLSurfaceView.Renderer {
     @Override
     public void onDrawFrame(GL10 gl) {
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+
+        mTriangle.draw();  // Draw triangle
     }
+
+    public static int loadShader(int type, String shaderCode){
+
+        // create a vertex shader type (GLES20.GL_VERTEX_SHADER)
+        // or a fragment shader type (GLES20.GL_FRAGMENT_SHADER)
+        int shader = GLES20.glCreateShader(type);
+
+        // add the source code to the shader and compile it
+        GLES20.glShaderSource(shader, shaderCode);
+        GLES20.glCompileShader(shader);
+
+        return shader;
+    }
+
 }
