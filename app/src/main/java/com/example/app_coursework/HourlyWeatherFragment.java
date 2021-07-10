c799754839ee2894d442f5d18f2ef1dd99403803
Tom Carter <thomascsrter01@me.com>
2021-07-10 19:19:53 +0100

    Hourly weather element created for each hour in forecast

--- app/src/main/java/com/example/app_coursework/HourlyWeatherFragment.java
+++ app/src/main/java/com/example/app_coursework/HourlyWeatherFragment.java
@@ -25,6 +25,8 @@ import java.util.ArrayList;
 public class HourlyWeatherFragment extends Fragment {
 
     View v;
+    LinearLayout rootView;
+    HorizontalScrollView scrollView;
     ArrayList<String> weatherList = new ArrayList<>();
     private static HourlyWeatherFragment instance = null;
 
@@ -34,6 +36,12 @@ public class HourlyWeatherFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         v = inflater.inflate(R.layout.fragment_hourly, container, false);
         instance = this;
+
+        scrollView = v.findViewById(R.id.hourly_horizontal);
+
+        rootView = new LinearLayout(getContext());
+        rootView.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        rootView.setOrientation(LinearLayout.HORIZONTAL);
 
         return v;
     }
@@ -45,6 +53,8 @@ public class HourlyWeatherFragment extends Fragment {
     protected void parseJSON(JSONObject response) {
         weatherList.clear();
         try {
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            // Read JSON
             JSONArray arr = response.getJSONObject("data").getJSONArray("timelines").getJSONObject(1).getJSONArray("intervals");
             for (int i = 0; i < arr.length(); i++) {
                 String[] dateFormatted = arr.getJSONObject(i).getString("startTime").split("T|:00\\+");  // Get time from UTC timestamp string
@@ -59,33 +69,15 @@ public class HourlyWeatherFragment extends Fragment {
                         String.valueOf((int) arr.getJSONObject(i).getJSONObject("values").getDouble("precipitationProbability")) + "%" + "," +
                         sunrise[1] + "," + sunset[1] + "," +
                         String.valueOf((int) arr.getJSONObject(i).getJSONObject("values").getDouble("temperatureApparent")) + "°C");
+
+                // Draw each element
+                View view  = inflater.inflate(R.layout.hourly_forecast_element, rootView, false);
+                rootView.addView(view);
             }
+            scrollView.addView(rootView);
         } catch (JSONException e) {
             e.printStackTrace();
         }
-
-//        HourlyWeatherAdapter adapter = new HourlyWeatherAdapter (getActivity(), weatherList);
-//        listView.setAdapter(adapter);
-
-        drawElements();
-    }
-
-    private void drawElements() {
-        HorizontalScrollView scrollView = v.findViewById(R.id.hourly_horizontal);
-
-        LinearLayout rootView = new LinearLayout(getContext());
-        rootView.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
-        rootView.setOrientation(LinearLayout.HORIZONTAL);
-
-        LayoutInflater inflater = LayoutInflater.from(getContext());
-        for (int i = 0; i < 50; i++) {
-            View view  = inflater.inflate(R.layout.hourly_forecast_element, rootView, false);
-            // set item content in view
-            rootView.addView(view);
-        }
-
-
-        scrollView.addView(rootView);
     }
 
 }
